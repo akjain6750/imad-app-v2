5eaec448ce932e4b9b4775710742cb7a24ca70a1
akjain6750 <akjain6750@gmail.com>
2017-03-05 13:09:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-
-var articleone :{
-    title:'article one\Akshat jain',
-    heading:'article one',
-    date:'2 march 2017',
+var articleone:{
+       title:'article one\ Akshat jain';
+ heading:'article one';
+    date:'2 march 2017';
     content: "
          <p>
                     this is the sontent of first article.this is the sontent of first article.this is the sontent of first article.this is the sontent of first article.this is the sontent of first article.this is the sontent of first article.this is the sontent of first article.this is the sontent of first article.this is the sontent of first article.this is the sontent of first article.this is the sontent of first article.this is the sontent of first article.
@@ -63,7 +62,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createtemplate(articleone));
 });
 
 app.get('/article-two', function(req, res){
