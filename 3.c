81eb18ce244878ab83d719dcd4ea5693da3dcb1d
akjain6750 <akjain6750@gmail.com>
2018-01-11 00:55:13 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void  main()
+{
+  char i;
+  printf("enter any character");
+  scanf("%s",i);
+  if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u')
+    printf("vowel");
+    else
+    printf("consonant");
+}
