0c56eccca450914aa2e57648942df7032b97196d
akjain6750 <akjain6750@gmail.com>
2018-01-11 21:46:56 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -4,7 +4,7 @@ void main()
   int i;
   printf("enter year");
   scanf("%d",&i);
-  if(i%4==0 && i%100!=0 && i%400==0)
+  if(i%4==0 && i%100!=0 || i%400==0)
     printf("leap year");
   else
     printf("not leap year");
